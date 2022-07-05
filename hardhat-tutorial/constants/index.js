7edf936d0b5b56cfffb6a5c3e86a3e9d8051838a
tnsqjahong <cruseight47@naver.com>
2022-07-06 02:48:40 +0900

    add route

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,4 +1,4 @@
 const WHITELIST_CONTRACT_ADDRESS = "0x0E59fE5b0B31268A8a3dFa25aA6FBdf9BDA9E801";
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://nft-collection-blond.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
