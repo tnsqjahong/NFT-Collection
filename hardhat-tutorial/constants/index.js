8010534795d799f166da329ba2aaf6a1b647a420
tnsqjahong <cruseight47@naver.com>
2022-07-09 13:02:14 +0900

    change address

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,4 +1,4 @@
 const WHITELIST_CONTRACT_ADDRESS = "0x6B1d5Bd6d740fD453cb7d94b02125e3E074E4993";
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://nft-collection-blond.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
